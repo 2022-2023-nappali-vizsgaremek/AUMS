d9e1df2cf7a3a5e2a98406d66361435a0b1a8932
TaborosiBalazs <sz1tabist@vasvari.org>
2023-02-22 15:54:48 +0100

    role and user_role models and controllers added.

---
+++ backend/models/role.py
@@ -0,0 +1,11 @@
+from models import db
+
+class Role(db.Model):
+    id = db.Column(db.SmallInteger,
+        unique=True,
+        nullable=False,
+        primary_key=True,
+        autoincrement=True)
+
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    level = db.Column(db.SmallInteger, unique=True, nullable=False)
