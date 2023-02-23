76f4aa513c221da19b12cf269b41206e47edc52a
sz1magmat <sz1magmat@vasvari.org>
2023-02-23 12:36:44 +0100

    user_controller register basics

--- backend/app.py
+++ backend/app.py
@@ -3,24 +3,24 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 
-import controllers.user_controller
-import controllers.schedule_controller
-
 import controllers.role_controller
-import controllers.user_role_controller
-
 import controllers.card_controller
+import controllers.schedule_controller
+import controllers.user_role_controller
 import controllers.user_card_controller
 
 from controllers.index_controller import Index
+from controllers.user_controller import Register
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost:3306/aums"
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:root@mysql_aums:3306/aums"
 
 CORS(app)
 api = Api(app)
 api.add_resource(Index, '/')
+api.add_resource(Register, '/register')
 
 with app.app_context():
     db.init_app(app)
--- backend/controllers/user_controller.py
+++ backend/controllers/user_controller.py
@@ -1,1 +1,21 @@
 from models.user import User, db
+from flask_restful import Resource, reqparse
+
+parser = reqparse.RequestParser()
+parser.add_argument('address', type=str, help='User Address')
+parser.add_argument('username', type=str, help='User Username')
+parser.add_argument('password', type=str, help='User Password')
+parser.add_argument('last_name', type=str, help='User Last Name')
+parser.add_argument('first_name', type=str, help='User First Name')
+parser.add_argument('birth_date', type=str, help='User Birth Date')
+parser.add_argument('phone_number', type=str, help='User Phone Number')
+parser.add_argument('company_email', type=str, help='User Company Email')
+parser.add_argument('personal_email', type=str, help='User Personal Email')
+
+class Register(Resource):
+    def post(self):
+        args = parser.parse_args()
+
+        return {
+            'status': 'failed',
+            'message': 'Not Implemented Yet'}, 200
