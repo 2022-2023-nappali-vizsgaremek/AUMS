1787e9ffb3fbc87f179499a405e23d1a1575a18d
sz1magmat <sz1magmat@vasvari.org>
2023-02-25 12:29:36 +0100

    Db fix

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
+from models import db
 import imports_app as imp
 from create import api, app
+from sqlalchemy_utils import database_exists, create_database
 
 from controllers.index_controller import Index
 from controllers.user_controller import Register
@@ -10,6 +12,14 @@ api.add_resource(Register, '/register')
 docs = imp.FlaskApiSpec(app)
 docs.register(Index)
 docs.register(Register)
+
+with app.app_context():
+    db.init_app(app)
+
+    if not database_exists(db.engine.url):
+        create_database(db.engine.url)
+
+    db.create_all()
 
 if __name__ == '__main__':
     #imp.serve(app, host='0.0.0.0', port=5000)
