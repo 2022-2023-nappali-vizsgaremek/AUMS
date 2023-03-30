353b52d13abaec20072c9b48b4c7de8d217137b7
sz1latben <sz1latben@vasvari.org>
2023-03-30 07:53:28 +0200

    base files, bootstrap, images, fonts added

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+    <head>
+        <title>Login V1</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="../images/icons/favicon.ico"/>
+    </head>
+
+    
+</template>
+<script>
+</script>
