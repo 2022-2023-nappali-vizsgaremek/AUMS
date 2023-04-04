8e366834ab1f4f83d9f848695d03faccc07e14fe
sz1tabist <sz1tabist@vasvari.org>
2023-04-04 13:40:25 +0200

    navigation fix

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">AUMS</a>
+            <a class="navbar-brand" href="/login">AUMS</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,20 +5,15 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Navigation
-    },
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+   },
     {
       path: '/register',
       name: 'register',
       component: () => import('../views/RegisterView.vue')
-    },
-  {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue')
-  }]
+    }]
 })
 
 export default router
