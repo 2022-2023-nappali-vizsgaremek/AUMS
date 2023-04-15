693718c189d4cfbb771dfc5379784a917ad19d5d
Bence Lator <sz1latben@vasvari.org>
2023-04-15 11:52:59 +0200

    small changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,14 +5,19 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue')
-   },
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
+    },
     {
-      path: '/register',
-      name: 'register',
-      component: () => import('../views/RegisterView.vue')
+      path: '/',
+      name: 'home',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/RegisterView.vue')
     }]
 })
 
