45fc1b004bb9b783689d8c95976c922757a69a3d
sz1latben <sz1latben@vasvari.org>
2023-04-25 10:11:56 +0200

    axios schedule get, random colors for schedules

--- frontend/src/views/ScheduleView.vue
+++ frontend/src/views/ScheduleView.vue
@@ -10,7 +10,8 @@
   </template>
   
   <script>
-  import {DayPilot, DayPilotCalendar, DayPilotNavigator} from '@daypilot/daypilot-lite-vue'
+  import {DayPilot, DayPilotCalendar, DayPilotNavigator} from '@daypilot/daypilot-lite-vue';
+  import axios from 'axios';
   var date = (new Date()).toISOString().split('T')[0];
   export default {
     name: 'Calendar',
@@ -51,9 +52,30 @@
       }
     },
     methods: {
-      loadEvents() {
-        // placeholder for an HTTP call
-        const events = [
+      async loadEvents() {
+        const response = await axios.get('http://127.0.0.1:5000/schedule')
+        let colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		                  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+                      '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+                      '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+                      '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+                      '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+                      '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+                      '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+                      '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+                      '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+        const raw_events = response.data;
+        let events = []; 
+        for (let i = 0; i < raw_events.length; i++) {
+          let event = {
+            id: raw_events[i].id,
+            start: raw_events[i].start,
+            end: raw_events[i].end,
+            backColor: colors[Math.floor(Math.random() * 3) + 1]
+          }
+          events.push(event);
+        }
+        /*const events = [
           {
             id: 1,
             start: "2023-04-24T10:00:00",
@@ -78,7 +100,7 @@
             backColor: "#cc4125",
             borderColor: "#990000",
           }
-        ];
+        ];*/
         this.calendar.update({events});
       },
     },
