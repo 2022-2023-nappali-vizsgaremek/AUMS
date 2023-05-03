79fbd9e87bfdeec7df3d87b4b55c2dd98bba51ab
Bence Lator <sz1latben@vasvari.org>
2023-05-03 23:11:07 +0200

    some changes

--- frontend/src/views/ScheduleView.vue
+++ frontend/src/views/ScheduleView.vue
@@ -1,5 +1,13 @@
 <template>
-    <div class="wrap">
+    <div class="mb-3">
+      <select class="form-select" id="userSelect" required v-model="selectedUser">
+        <option disabled value="">Please select a user</option>
+        <option v-for="user in users" :key="user.id" :value="user.id">
+          {{ user.name }}
+        </option>
+      </select>
+    </div>
+    <div class="wrap" hidden>
       <div class="left">
         <DayPilotNavigator id="nav" :config="navigatorConfig" />
       </div>
@@ -39,6 +47,7 @@
 
     data: function() {
 
+      const users = ref([]);
       var date = (new Date()).toISOString().split('T')[0];
       const info = ref('');
       const modalActive = ref(false);
@@ -53,6 +62,8 @@
       };
 
       return {
+        users,
+        selectedUser: '',
         info,
         date,
         modalActive,
@@ -102,8 +113,43 @@
       }
     },
     methods: {
+      async loadUsers() {
+        const response = await axios.get('http://127.0.0.1:5000/users');
+        this.users.value = response.data;
+        console.log(this.users.value);
+      },
+      async loadEventsByUser() {
+        let events = [];
+        const response = await axios.get('http://127.0.0.1:5000/schedule');
+        const raw_events = response.data;
+        let colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+		                  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+                      '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+                      '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+                      '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+                      '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+                      '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+                      '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+                      '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+                      '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
+        for (const event of raw_events) {
+          if (event.user_id == userid) {
+            let color = colors[Math.floor(Math.random() * colors.length)];
+            events.push({
+              id: event.id,
+              start: event.start,
+              end: event.end,
+              color: color
+            });
+          }
+        }
+        console.log(events);
+      },
+
+
       async loadEvents() {
-        const response = await axios.get('http://127.0.0.1:5000/schedule')
+        const response = await axios.get('http://127.0.0.1:5000/schedule');
         let colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
 		                  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
                       '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
@@ -121,7 +167,7 @@
             id: raw_events[i].id,
             start: raw_events[i].start,
             end: raw_events[i].end,
-            backColor: colors[Math.floor(Math.random() * 50) + 1]
+            backColor: colors[Math.floor(Math.random() * colors.length) + 1]
           }
           events.push(event);
         }
@@ -156,7 +202,8 @@
       
     },
     mounted() {
-      this.loadEvents();
+      //this.loadEvents();
+      this.loadUsers();
     }
   }
   </script>
