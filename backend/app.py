b98e90dbab82a9da0dade86c9e6c9e38076a0432
sz1magmat <sz1magmat@vasvari.org>
2023-05-03 07:42:26 +0200

    Frontend menu protection, login w/ tokens, def user

--- backend/app.py
+++ backend/app.py
@@ -19,6 +19,7 @@ import controllers.schedule_controller as schedule_ctrl
 
 try:
     # External imports
+    import bcrypt
     from waitress import serve
     from sqlalchemy_utils import database_exists
     from sqlalchemy_utils import create_database
@@ -51,6 +52,7 @@ api.add_resource(card_ctrl.CardValidation, "/card_validation/<string:card_number
 docs = FlaskApiSpec(app)
 # Index docs
 docs.register(index_ctrl.Index)
+api.add_resource(index_ctrl.IsAuthenticated, "/is_authenticated/<string:access_token>")
 # User docs
 docs.register(user_ctrl.Login)
 docs.register(user_ctrl.Register)
@@ -72,14 +74,29 @@ with app.app_context():
     db.init_app(app)
     engine = db.engine.url
 
-    if not database_exists(db.engine.url):
-        create_database(db.engine.url)
-
     try:
+        created_now = False
         if not database_exists(engine):
             create_database(engine)
+            created_now = True
 
         db.create_all()
+
+        if created_now:
+            from models.user import User
+            user = User(
+                first_name="Admin",
+                last_name="Admin",
+                birth_date="2000-01-01",
+                phone_number="36203344567",
+                address="Szeged",
+                company_email="admin.admin@proj-aums.hu",
+                personal_email="postmaster@proj-aums.hu",
+                username="admin.admin",
+                password=bcrypt.hashpw("admin".encode("utf-8"), bcrypt.gensalt()).decode("utf-8"))
+            db.session.add(user)
+            db.session.commit()
+
         log.info("Db created")
     except Exception as db_error:
         exit_app(f"Db error: {db_error}")
